c249692703461725fc5d4a1dd0f720d340fe44a3
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-15 14:29:58 +0530

    01

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-
-    react()],
+  plugins: [tailwindcss(),react()],
     
-  
+  base:"/Reomotework"
 })
 
