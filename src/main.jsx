07597417394ad3392d7b0f4398bee6708ddb2a0f
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-16 10:37:17 +0530

    gh

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,12 @@ import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css'
 import './index.css'
 import './polyfills/globalish';
-import { HashRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </HashRouter>
+  </StrictMode>,
 )
-
 
