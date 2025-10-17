7d6744b13bb735ab12492a825c93c6aee53bac23
anjupavithran123 <76482227+anjupavithran123@users.noreply.github.com>
2025-10-17 12:10:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css'
 import './index.css'
+import './polyfills/globalish';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter >
     <App />
-  </StrictMode>,
-)
+  </HashRouter>
+);
