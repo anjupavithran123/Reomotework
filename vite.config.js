02d175c854ffdd2176eb0935c2247cc8dfbf358c
anjupavithran123 <76482227+anjupavithran123@users.noreply.github.com>
2025-10-17 14:46:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
   define: {
     // replace global references at build-time with globalThis
     global: 'globalThis',
@@ -13,7 +12,5 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  base: process.env.NODE_ENV === 'production' ? '/Reomotework/' : '/'
-})
-})
-
+  base: process.env.NODE_ENV === 'production' ? '/Reomotework/' : '/',
+});
