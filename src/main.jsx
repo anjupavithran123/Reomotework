2316e0741461ea93360fde3d542aba59a240fb28
anjupavithran123 <76482227+anjupavithran123@users.noreply.github.com>
2025-10-17 11:52:23 +0530

    Replace HashRouter with StrictMode in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css'
 import './index.css'
-import './polyfills/globalish';
-import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-  <HashRouter >
-=======
-  <HashRouter  basename="/Reomotework">
->>>>>>> origin/master
+  <StrictMode>
     <App />
-  </HashRouter>
-);
+  </StrictMode>,
+)
