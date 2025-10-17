c0a485d41318ea0947fbf7233b89d2eb9b5b7199
anjupavithran123 <76482227+anjupavithran123@users.noreply.github.com>
2025-10-17 12:11:44 +0530

    Replace anchor tags with React Router Links

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import { FaBars } from "react-icons/fa";
 import axios from "axios";
 import ProfilepicUpload from "./profilepic";
 import { io } from "socket.io-client";
+import { Link } from "react-router-dom";
+
 
 const SERVER_URL = "http://localhost:4001"; // for doc/taskboard
 const CHAT_SERVER_URL = "http://localhost:3002"; // new
@@ -142,33 +144,33 @@ function Home() {
             </div>
 
             <ul className={isOpen ? "nav-link active" : "nave-link"}>
-              <li><a href="/members">Members</a></li>
+              <li><Link to="/members">Members</Link></li>
 
               <li>
-                <a href="/chatcontainer" onClick={handleChatClick} className="docs-link">
+                <Link to="/chatcontainer" onClick={handleChatClick} className="docs-link">
                   ChatBox
                   {chatNotifications > 0 && <span className="badge">{chatNotifications}</span>}
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a href="/document" onClick={handleDocsClick} className="docs-link">
+               <Link to="/document" onClick={handleDocsClick} className="docs-link">
                   Docs Editor
                   {docsBadgeCount > 0 && <span className="badge">{docsBadgeCount}</span>}
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a href="/taskboard" onClick={handleTaskboardClick} className="docs-link">
+               <Link to="/taskboard" onClick={handleTaskboardClick} className="docs-link">
                   Taskboard
                   {taskboardBadgeCount > 0 && <span className="badge">{taskboardBadgeCount}</span>}
-                </a>
+                </Link>
               </li>
 
-              <li><a href="/fileupload">Fileupload</a></li>
-              <li><a href="/whiteboard">White-Board</a></li>
-              <li><a href="/email">Invite email</a></li>
-              <li><a href="/videocall">Video-Chat</a></li>
+              <li><Link to="/fileupload">Fileupload</Link></li>
+              <li><Link to="/whiteboard">White-Board</Link></li>
+              <li><Link to="/email">Invite email</Link></li>
+              <li><Link to="/videocall">Video-Chat</Link></li>
             </ul>
 
             <div className="icon" onClick={toggleMenu}><FaBars /></div>
