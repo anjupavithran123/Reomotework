0901dde5cf31660639a8f5e60db835137726f895
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-19 15:32:17 +0530

    Resolve merge conflicts

--- src/chatcontainer.jsx
+++ src/chatcontainer.jsx
@@ -7,7 +7,7 @@ import socketIOClient from "socket.io-client";
 
 const ChatContainer = () => {
   const [user, setUser] = useState(localStorage.getItem("user"));
-  const socketio = socketIOClient("http://localhost:3002");
+  const socketio = socketIOClient("https://chat-server-7znj.onrender.com");
   const [chats, setChats] = useState([]);
 
   useEffect(() => {
