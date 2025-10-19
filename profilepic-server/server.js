de72c0e3ea59b9ddd39e7a469234551ac463a705
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-19 19:14:11 +0530

    Resolve

--- profilepic-server/server.js
+++ profilepic-server/server.js
@@ -32,4 +32,4 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
