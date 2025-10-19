d7359cd877546506be6825267be06f02cc9a729c
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-19 23:12:34 +0530

    Resolve

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 
 export const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-// export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:4000';
-export const UPLOAD_URL = process.env.REACT_APP_UPLOAD_URL || 'http://localhost:4002';
-export const EMAIL_URL = process.env.REACT_APP_EMAIL_URL || 'http://localhost:4003';
-export const DOCUMENT_URL = process.env.REACT_APP_DOCUMENT_URL || 'http://localhost:4001';
+// export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
+export const UPLOAD_URL = process.env.REACT_APP_UPLOAD_URL || 'http://localhost:3001';
+export const EMAIL_URL = process.env.REACT_APP_EMAIL_URL || 'http://localhost:3001';
+export const DOCUMENT_URL = process.env.REACT_APP_DOCUMENT_URL || 'http://localhost:3001';
 
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+
+export const socket = io("https://signup-server-1.onrender.com", {
   transports: ["websocket"],
   autoConnect: false, // connect manually
 });
