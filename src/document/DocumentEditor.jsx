e5a8918c1b2e40f9321684a37c55b714aa7e27f5
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-19 19:53:27 +0530

    Resolve

--- src/document/DocumentEditor.jsx
+++ src/document/DocumentEditor.jsx
@@ -3,7 +3,7 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import { io } from 'socket.io-client';
 
-const SERVER_URL = 'https://document-server-osvl.onrender.com';
+const SERVER_URL = 'https://document-server1.onrender.com';
 
 
 export default function DocumentEditor({ docId = 'default-doc', username = 'Anonymous' }) {
