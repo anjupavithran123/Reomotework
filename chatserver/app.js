df908de60b2beed13f0b33c3efb74d4cd29051b7
anjupavithran123 <anjupavithranm95@gmail.com>
2025-10-20 11:09:16 +0530

    first

--- chatserver/app.js
+++ chatserver/app.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
         origin: "*"
     }
 })
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 io.on("connection", (socket) => {
     console.log("connected");
---
+++ src/config.js
@@ -0,0 +1,9 @@
+
+
+
+// export const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+// // export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
+// export const UPLOAD_URL = process.env.REACT_APP_UPLOAD_URL || 'http://localhost:3001';
+// export const EMAIL_URL = process.env.REACT_APP_EMAIL_URL || 'http://localhost:3001';
+// export const DOCUMENT_URL = process.env.REACT_APP_DOCUMENT_URL || 'http://localhost:3001';
+
